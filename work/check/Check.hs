14aa70894e20c7a737801fe83b1b7e94dd6a83c9
Jonas Fiala <jf613@cam.ac.uk>
2019-11-05 14:36:13 +0000

    Update to return list of actions

--- work/check/Check.hs
+++ work/check/Check.hs
@@ -2,26 +2,26 @@ import Blarney
 
 data Prop = Assert (Bit 1) | Forall (Bit 3 -> Prop)
 
--- String is just for debugging, Integer tracks what case we are on
-checkGenerate :: String -> Integer -> Integer -> Prop -> [Recipe]
-checkGenerate s _ _ (Assert prop) = [Action do
-                              display "Test" s ", result: " (prop)
-                              ]
-checkGenerate s maxDepth currDepth (Forall f)
-  | currDepth >= maxDepth = appliedForall
-  | otherwise             = appliedForall ++ checkGenerate s maxDepth (currDepth+1) (Forall f)
-  where
-    appliedForall = checkGenerate (s ++ " " ++ show currDepth) maxDepth 0 (f (constant currDepth))
-
-check :: Integer -> Prop -> [Recipe]
+check :: Integer -> Prop -> [Action ()]
 check depth prop = (checkGenerate "" depth 0 prop)
+    where
+      -- String is just for debugging, Integer tracks what case we are on
+      checkGenerate :: String -> Integer -> Integer -> Prop -> [Action ()]
+      checkGenerate s _ _ (Assert prop) = [do
+                                    display "Test" s ", result: " (prop)
+                                    ]
+      checkGenerate s maxDepth currDepth (Forall f)
+        | currDepth >= maxDepth = appliedForall
+        | otherwise             = appliedForall ++ checkGenerate s maxDepth (currDepth+1) (Forall f)
+        where
+          appliedForall = checkGenerate (s ++ " " ++ show currDepth) maxDepth 0 (f (constant currDepth))
 
 
 top :: Module ()
 top = do
   let propSubComm = Forall \a -> Forall \b -> Assert (a.-.b.==.b.-.a)
   
-  let testSeq = Par (check (2^3-1) propSubComm)
+  let testSeq = Par (map Action (check (2^3-1) propSubComm))
   done <- run (reg 1 0) testSeq
 
   globalTime :: Reg (Bit 32) <- makeReg 0
