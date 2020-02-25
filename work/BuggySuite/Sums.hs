cec7e99562b2ad6f4ba478e17b0582e844822c43
Jonas <jonas.fiala.cardiff@gmail.com>
2020-02-25 11:52:11 +0000

    Successful synthesis of long comb checking, takes 21s

--- work/BuggySuite/Sums.hs
+++ work/BuggySuite/Sums.hs
@@ -5,8 +5,8 @@ import Check.Check
 testBench :: Module ()
 testBench = do
   --let propSubComm = Forall "A" \a -> Forall "B" \b -> Assert ((a :: Bit 2)-b.==.b-a)
-  let prop_Associativity = ("Associativity", Forall \(x :: Bit 4) -> Forall \(y :: Bit 4) -> Forall \(z :: Bit 4) -> Assert ((x .+. y) .+. z .==. x .+. (y .+. z)))
-  let prop_Commutativity = ("Commutativity", Forall \(x :: Bit 4) -> Forall \(y :: Bit 4) -> Assert (x .+. y .==. y .+. x))
+  let prop_Associativity = ("Associativity", Forall \(x :: Bit 10) -> Forall \(y :: Bit 10) -> Forall \(z :: Bit 10) -> Assert ((x .+. y) .+. z .==. x .+. (y .+. z)))
+  let prop_Commutativity = ("Commutativity", Forall \(x :: Bit 15) -> Forall \(y :: Bit 15) -> Assert (x .+. y .==. y .+. x))
   
   _ <- check noAction [prop_Associativity, prop_Commutativity] 0
   
--- work/CheckFPGA/Src/Main.hs
+++ work/CheckFPGA/Src/Main.hs
@@ -7,8 +7,8 @@ testBench :: Stream (Bit 8) -> Module (Stream (Bit 8))
 testBench bytesIn = do  
   bytesOut :: Queue (Bit 8) <- makeQueue
   
-  let prop_Associativity = ("Associativity", Forall \(x :: Bit 4) -> Forall \(y :: Bit 4) -> Forall \(z :: Bit 4) -> Assert ((x .+. y) .+. z .==. x .+. (y .+. z)))
-  let prop_Commutativity = ("Commutativity", Forall \(x :: Bit 4) -> Forall \(y :: Bit 4) -> Assert (x .+. y .==. y .+. x))
+  let prop_Associativity = ("Associativity", Forall \(x :: Bit 10) -> Forall \(y :: Bit 10) -> Forall \(z :: Bit 10) -> Assert ((x .+. y) .+. z .==. x .+. (y .+. z)))
+  let prop_Commutativity = ("Commutativity", Forall \(x :: Bit 15) -> Forall \(y :: Bit 15) -> Assert (x .+. y .==. y .+. x))
  
   _ <- check noAction [prop_Associativity, prop_Commutativity] 0 bytesOut
 
