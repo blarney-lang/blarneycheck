615d2ce447e68986fb820a12e8cbc60b541d4330
Jonas <jonas.fiala.cardiff@gmail.com>
2020-04-30 12:06:10 +0100

    Add new FPGA synthesizable folder, using PLL

---
+++ work/CheckFPGA_Sums/Src/Main.hs
@@ -0,0 +1,30 @@
+-- Blarney imports
+import Blarney
+import Blarney.Queue
+import Blarney.Stream
+import Check.Check
+
+testBench :: Stream (Bit 8) -> Module (Stream (Bit 8))
+testBench bytesIn = do  
+  bytesOut :: Queue (Bit 8) <- makeQueue
+  
+  let prop_Associativity = Forall \(x :: Bit 10) -> Forall \y -> Forall \z -> Assert ((x .+. y) .+. z .==. x .+. (y .+. z))
+  let prop_Commutativity = Forall \(x :: Bit 15) -> Forall \y ->              Assert (x .+. y .==. y .+. x)
+  
+  let properties = [
+          ("Associativity", prop_Associativity)
+        , ("Commutativity", prop_Commutativity)
+        ]
+
+  checkPure properties >>= outputForSynthesis bytesOut
+
+  always do
+    when (bytesIn.canPeek) do
+      bytesIn.consume
+
+  return (bytesOut.toStream)
+
+
+main :: IO ()
+main = do
+  writeVerilogModule testBench "Top" "Top-Verilog/"
